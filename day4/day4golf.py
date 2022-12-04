d63c0344b31f71aa00d4b2e555d818884140bc54
Pyroan <Pyroan@users.noreply.github.com>
2022-12-04 15:32:32 -0800

    :golf: day 4: -56 bytes. Probably should stop lol

--- day4/day4golf.py
+++ day4/day4golf.py
@@ -1,4 +1,4 @@
-f=lambda a,b:{*range(a,b+1)}
-g=lambda a,b:(a&b in[a,b])+(a^b<a|b)*1000
-c=sum(g(*[f(*map(int,e.split('-')))for e in p.split(',')])for p in open('day4.txt'))
-print(c%1000,c//1000)
+import re
+t,s=0,0
+for p in open('day4.txt'):a,b,c,d=map(int,re.split(',|-',p));t+=b-c>=0<=d-a;s+=(a-c)*(b-d)<=0
+print(s,t)
