d6799115189eb3aeef479d6ab6012d42398cfc2e
Pyroan <Pyroan@users.noreply.github.com>
2022-12-04 12:16:11 -0800

    :golf: day4: -8 bytes

--- day4/day4golf.py
+++ day4/day4golf.py
@@ -1,4 +1,4 @@
-f=lambda x:range(*map(int,x.split('-')))
+f=lambda a,b:{*range(a,b+1)}
 g=lambda a,b:(a&b in[a,b])+(a^b!=a|b)*1000
-c=sum(g(*[set(f(e))|{f(e).stop}for e in p.split(',')])for p in open('day4.txt'))
+c=sum(g(*[f(*map(int,e.split('-')))for e in p.split(',')])for p in open('day4.txt'))
 print(c%1000,c//1000)
