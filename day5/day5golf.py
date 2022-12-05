47452a11e25cf3341b5f8582b0ab230734d1e676
Pyroan <Pyroan@users.noreply.github.com>
2022-12-05 15:43:28 -0800

    :golf::five: -5bytes

--- day5/day5golf.py
+++ day5/day5golf.py
@@ -1,10 +1,7 @@
-l=[x[:-1]for x in open('day5.txt')]
-u=l.index('')
-c,p=l[:u-1],l[u+1:]
+l=open('day5.txt').readlines()
+u=l.index('\n')
 for y in[-1,1]:
- a=(len(c[0])+8)//4*['']
- for w in c:
-  i=1
-  for v in w[1::4]:a[i]+=v*(v!=' ');i+=1
- for z in p:n,h,t=map(int,z.split()[1::2]);a[t]=a[h][:n][::y]+a[t];a[h]=a[h][n:]
- print(*[s[0]for s in a[1:]],sep='')
+ a=(len(l[0])//4+1)*[[]]
+ for i in range(len(a)-1):a[i+1]=[x for w in l[:u-1]if(x:=w[1+i*4])!=' ']
+ for z in l[u+1:]:n,h,t=map(int,z.split()[1::2]);a[t]=a[h][:n][::y]+a[t];a[h]=a[h][n:]
+ print(''.join(s[0]for s in a[1:]))
